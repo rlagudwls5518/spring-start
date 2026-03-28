390bac1e0b07dc7762c35c39684d0291909eacb5
rlagudwls5518 <rlagudwls5518@naver.com>
2026-03-28 12:27:07 +0900

    feat : 회원 등록 폼 개발

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -3,13 +3,20 @@ package hello.hello_spring.controller;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
 
-    private MemberService memberService;
+    private final MemberService memberService;
+
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
     }
 }
