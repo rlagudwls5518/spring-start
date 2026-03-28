fb0ab8839bf811a416498d77cea29945ae2d7627
rlagudwls5518 <rlagudwls5518@naver.com>
2026-03-28 14:52:31 +0900

    feat : JDBC 리포지토리 구현

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -3,11 +3,20 @@ package hello.hello_spring;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
         return new MemoryMemberRepository();
     }
 }
