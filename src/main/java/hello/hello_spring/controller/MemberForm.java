b524b75181011718e15b6cc015c790f148b9da72
rlagudwls5518 <rlagudwls5518@naver.com>
2026-03-28 12:38:49 +0900

    feat : 회원 등록 폼 개발2

---
+++ src/main/java/hello/hello_spring/controller/MemberForm.java
@@ -0,0 +1,17 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.service.MemberService;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
